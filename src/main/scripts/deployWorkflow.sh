e8d963cd715bcb0d102403e355a9668ee662d1d2
Joseph D McTee <mcjoe@jeklsoft.com>
2014-03-02 18:21:39 -0700

    Added hive actions with example fork/join.

--- src/main/scripts/deployWorkflow.sh
+++ src/main/scripts/deployWorkflow.sh
@@ -8,6 +8,10 @@ hadoop fs -rm -r hdfs://hadoop-nn:8020/workflows/pig
 hadoop fs -mkdir hdfs://hadoop-nn:8020/workflows/pig
 hadoop fs -put src/main/pig/*.pig hdfs://hadoop-nn:8020/workflows/pig/
 
+hadoop fs -rm -r hdfs://hadoop-nn:8020/workflows/hive
+hadoop fs -mkdir hdfs://hadoop-nn:8020/workflows/hive
+hadoop fs -put src/main/hive/*.hql hdfs://hadoop-nn:8020/workflows/hive/
+
 hadoop fs -rm -r hdfs://hadoop-nn:8020/workflows/workflow
 hadoop fs -mkdir hdfs://hadoop-nn:8020/workflows/workflow
 hadoop fs -put src/main/workflows/workflow.xml hdfs://hadoop-nn:8020/workflows/workflow/
