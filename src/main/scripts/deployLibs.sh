697bc48d3029807da77ab538fa84aa7eb2feecbc
Joseph D McTee <mcjoe@jeklsoft.com>
2014-03-04 12:33:09 -0700

    Final oozie sqoop support changes.

--- src/main/scripts/deployLibs.sh
+++ src/main/scripts/deployLibs.sh
@@ -6,3 +6,4 @@ hadoop fs -rm -r hdfs://hadoop-nn:8020/workflows
 hadoop fs -mkdir hdfs://hadoop-nn:8020/workflows
 hadoop fs -mkdir hdfs://hadoop-nn:8020/workflows/lib
 hadoop fs -put build/output/lib/*.jar hdfs://hadoop-nn:8020/workflows/lib/
+hadoop fs -put binaries/postgresql-9.1-903.jdbc4.jar hdfs://hadoop-nn:8020/workflows/lib/
